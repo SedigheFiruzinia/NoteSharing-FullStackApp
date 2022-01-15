4b4ebb798bce0da8c41cccd5b81aa06225e194f2
SedigheFiruzinia <sedighefiruzinia@gmail.com>
2022-01-15 22:58:52 +0200

    push

--- controllers/login.js
+++ controllers/login.js
@@ -1,28 +1,30 @@
-const loginRouter = require('express').Router()
-const userSchema = require('../schema/user')
-const bcryptjs = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-const config = require('../utils/config')
+const loginRouter = require("express").Router();
+const userSchema = require("../schema/user");
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("../utils/config");
 
+loginRouter.post("/", async (request, response) => {
+  const user = await userSchema.findOne({ username: request.body.username });
+  const passwordCorrect =
+    user === null
+      ? false
+      : await bcryptjs.compare(request.body.password, user.passwordHash);
+  // how passwordhash returns
+  if (!(user && passwordCorrect)) {
+    return response.status(401).json({ error: "invalid username or password" });
+  }
 
-loginRouter.post('/', async (request, response) => {
-    console.log(' request.body.username',request.body.username)
-    const user = await userSchema.findOne({ username: request.body.username })
-    const passwordCorrect = user === null ? false
-        : await bcryptjs.compare(request.body.password, user.passwordHash)
-        // how passwordhash returns
-    if (!(user && passwordCorrect)) {
-        return response.status(401).json({ error: 'invalid username or password' })
-    }
+  const userForToken = {
+    username: user.username,
+    id: user._id,
+  };
 
-    const userForToken = {
-        username: user.username,
-        id: user._id
-    }
+  const token = jwt.sign(userForToken, config.SECRET);
 
-    const token = jwt.sign(userForToken, config.SECRET)
+  response
+    .status(200)
+    .send({ token, username: user.username, name: user.name, id: user._id });
+});
 
-    response.status(200).send({ token, username: user.username, name: user.name, id: user._id })
-})
-
-module.exports = loginRouter
+module.exports = loginRouter;
--- schema/note.js
+++ schema/note.js
@@ -1,32 +1,33 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const noteSchema = new mongoose.Schema({
   owner: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'Users'
+    ref: "Users",
   },
   text: {
     type: String,
-    required: true
+    required: true,
   },
   likes: {
     type: Number,
-    required: [true, 0]
-  }, 
-  sharedWith: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Users'
-  }]
-  ,
+    required: [true, 0],
+  },
+  sharedWith: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Users",
+    },
+  ],
   //comments: [{body:"string", by: mongoose.Schema.Types.ObjectId}]
-})
+});
 
-noteSchema.set('toJSON', {
+noteSchema.set("toJSON", {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 
-module.exports = mongoose.model('Notes', noteSchema)
+module.exports = mongoose.model("Notes", noteSchema);
